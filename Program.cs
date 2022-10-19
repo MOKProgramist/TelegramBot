1b8ea4563da10770502a700d5a12057fbd2af090
Hi, I'm ANDProgrammist ! <92784790+MOKProgramist@users.noreply.github.com>
2022-10-19 10:21:41 +0300

    Много всего. А именно: `*` Вынесли команды в отдельную папку. `*` Что-то еще!)Create README.md

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
+using TelegramBot.commands;
+
 var botClient = new TelegramBotClient("5459925148:AAHf1xcPBrwFHo_Q9MuNt_TuTCGgElDH1qE");
 
 using var cts = new CancellationTokenSource();
@@ -33,6 +35,7 @@ cts.Cancel();
 
 async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
 {
+    Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
     // Only process Message updates: https://core.telegram.org/bots/api#message
     if (update.Message is not { } message)
         return;
@@ -40,44 +43,22 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
     if (message.Text is not { } messageText)
         return;
 
+    int messageId = message.MessageId | 0;
+
     var chatId = message.Chat.Id;
     var userInfo = $"{message?.From?.FirstName} {message?.From?.LastName}" ?? "Не указано";
 
     Console.WriteLine($"Received a '{messageText}' message in chat {chatId} ({userInfo}).");
 
-    //// Echo received message text
-    //Message sentMessage = await botClient.SendTextMessageAsync(
-    //    chatId: chatId,
-    //    text: "Твое сообщение:\n" + messageText,
-    //    replyToMessageId: message.MessageId,
-    //    cancellationToken: cancellationToken);
-    //// send sticker
-    //Message sendSticer = await botClient.SendStickerAsync(chatId: chatId, sticker: "https://www.rbauto.ru/upload/resize_cache/webp/upload/iblock/df0/df07731a4ae4504ff073c635137afb5f.webp");
-    //// button
-    //await botClient.SendTextMessageAsync(chatId, messageText, replyMarkup: replyKeyboardMarkup());
+    // Начальная команда
     if (messageText == "/start")
     {
-        await botClient.SendTextMessageAsync(
-        chatId: chatId,
-        text: $"Добро пожаловать, {userInfo}, В нашего бота по отправки картинок животных :)",
-        replyToMessageId: message.MessageId,
-        replyMarkup: replyKeyboardMarkup(),
-        cancellationToken: cancellationToken);
-        return;
+        await Starts.Start(botClient, message, cancellationToken);
     }
-    else if (messageText.ToLower() == "котик") 
+    else if (messageText.ToLower() == "котик")
     {
-        await botClient.SendPhotoAsync(
-            chatId: chatId,
-            photo: "https://krasivosti.pro/uploads/posts/2021-07/1625880574_16-krasivosti-pro-p-milie-parnie-kotiki-koti-krasivo-foto-16.jpg",
-            caption: "<b>Котик</b>. <i>Source</i>: <a href=\"https://krasivosti.pro\">Pixabay</a>",
-        parseMode: ParseMode.Html,
-
-            cancellationToken: cancellationToken);
+        await Animals.Cat(botClient, message, cancellationToken);
         return;
-    } else if(messageText.ToLower() == "собака")
-    {
-
     }
     else
     {
@@ -89,16 +70,7 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
     }
 }
 
-IReplyMarkup? replyKeyboardMarkup()
-{
-    return new ReplyKeyboardMarkup(new[]
-        {
-        new KeyboardButton[] { "Котик", "Рыбка" },
-        new KeyboardButton[] { "Собака", "Цветок" },
-    });
-}
-
-    Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
 {
     var ErrorMessage = exception switch
     {
---
+++ commands/Starts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramBot.commands
+{
+    internal class Starts
+    {
+        static public async Task Start(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
+        {
+            int messageId = message.MessageId | 0;
+            var chatId = message.Chat.Id;
+            var userInfo = $"{message?.From?.FirstName} {message?.From?.LastName}" ?? "Не указано";
+
+            await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: $"Добро пожаловать, {userInfo}, В нашего бота по отправки картинок животных :)",
+                replyToMessageId: messageId,
+                replyMarkup: keyboardStartUser(),
+                cancellationToken: cancellationToken
+            );
+        }
+
+        static IReplyMarkup? keyboardStartUser()
+        {
+            return new ReplyKeyboardMarkup(new[]
+                {
+        new KeyboardButton[] { "Котик", "Рыбка" },
+        new KeyboardButton[] { "Собака", "Цветок" },
+                });
+        }
+    }
+}
