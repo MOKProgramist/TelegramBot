a18424dec54b33778b0a0575eef4734d731771b8
323 <323@323-1>
2022-12-21 10:24:02 +0300

    Новые интерфейсы, нужно делать строгую типизацию

---
+++ core/types/IContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.core.types
+{
+    internal interface IContext
+    {
+        string Text { get; set; } // название
+    }
+}
---
+++ core/types/IContextCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.core.types
+{
+    internal interface IContextCommand
+    {
+        string Command { get; set; } // команда
+        string[] Body { get; set; } // команда
+    }
+}
